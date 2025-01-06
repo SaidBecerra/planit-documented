564f4e2da4cc0689c24f7e5c772378a959bb52f0
omarl <omarlahmimi@gmail.com>
2025-01-05 23:00:17 -0500

    add dislikes screen

---
+++ lib/widgets/registration/dislikes_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DislikesScreen extends StatefulWidget {
+  const DislikesScreen({super.key});
+
+  @override
+  State<DislikesScreen> createState() {
+    return _DislikesScreenState();
+  }
+}
+
+class _DislikesScreenState extends State<DislikesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
