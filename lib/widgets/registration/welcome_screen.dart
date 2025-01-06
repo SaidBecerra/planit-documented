1f6d48316cc59770bb47ba4bc2f823add56e10b8
omarl <omarlahmimi@gmail.com>
2025-01-05 22:55:39 -0500

    make main page scrollable

--- lib/widgets/registration/welcome_screen.dart
+++ lib/widgets/registration/welcome_screen.dart
@@ -18,88 +18,90 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5EFFF),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 400,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  borderRadius: BorderRadius.circular(40),
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              TitleText(text: 'Let\'s Get Started!'),
-              SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                  width: 350,
-                  child: NormalText(
-                    alignment: TextAlign.center,
-                    text:
-                        'Create a PlanIt account or login if you already have an account',
-                  )),
-              SizedBox(height: 40),
-              MainButton(
-                text: 'Register with Email',
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
-                image: Image.asset(
-                  'assets/images/mail.png',
-                  width: 20,
-                  color: Colors.white,
-                ),
-                onTap: () {
-                  _onRegister(context);
-                },
-              ),
-              SizedBox(height: 20),
-              NormalText(
-                text: 'Or',
-                alignment: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              MainButton(
-                text: 'Continue with Google',
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.black,
-                image: Image.asset(
-                  'assets/images/google.png',
-                  width: 20,
-                ),
-                onTap: () {
-                  _onRegister(context);
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  NormalText(
-                    text: 'Already have an Account?',
-                    alignment: TextAlign.center,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(40),
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Login',
-                      style: GoogleFonts.lato(
-                        color: Color(0xFFA294F9),
-                        fontSize: 20,
-                      ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                TitleText(text: 'Let\'s Get Started!'),
+                SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                    width: 350,
+                    child: NormalText(
+                      alignment: TextAlign.center,
+                      text:
+                          'Create a PlanIt account or login if you already have an account',
+                    )),
+                SizedBox(height: 40),
+                MainButton(
+                  text: 'Register with Email',
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  image: Image.asset(
+                    'assets/images/mail.png',
+                    width: 20,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    _onRegister(context);
+                  },
+                ),
+                SizedBox(height: 20),
+                NormalText(
+                  text: 'Or',
+                  alignment: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                MainButton(
+                  text: 'Continue with Google',
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.black,
+                  image: Image.asset(
+                    'assets/images/google.png',
+                    width: 20,
+                  ),
+                  onTap: () {
+                    _onRegister(context);
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    NormalText(
+                      text: 'Already have an Account?',
+                      alignment: TextAlign.center,
                     ),
-                  )
-                ],
-              )
-            ],
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Login',
+                        style: GoogleFonts.lato(
+                          color: Color(0xFFA294F9),
+                          fontSize: 20,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
